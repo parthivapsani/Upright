413caf9043673aceae9df19f55859ab6255367ff
Pranav Wadhwa <pranavwadhwa@outlook.com>
2021-02-13 14:41:10 -0500

    Added landing page

--- upright-app/main.js
+++ upright-app/main.js
@@ -47,32 +47,51 @@ function loadMenuBar() {
 
 function loadOnboarding() {
 	var mainWindow = new BrowserWindow({
+		maxWidth: 1440,
+		maxHeight: 900,
 		webPreferences: {
 			nodeIntegration: true
 		}
 	});
-	mainWindow.loadURL('file://' + __dirname + '/src/views/onboarding.html');
+	mainWindow.maximize();
+	mainWindow.loadURL('file://' + __dirname + '/src/views/landing.html');
+
+	ipcMain.on('landing-next', (event, arg) => {
+		loadRegistration(mainWindow);
+	});
 
 	ipcMain.on('onboarding-completed', (event, arg) => {
 		const doc = {
 			onboarded: true
 		}
 		db.insert(doc);
+		loadMenuBar();
+		mainWindow.destroy();
 	});
 }
 
+function loadRegistration(window) {
+	window.loadURL('file://' + __dirname + '/src/views/registration.html');
+}
+
 function resetOnboarding() {
-	db.update({ onboarded: true}, { onboarded: false}, function (err, numReplaced) {
+	db.remove({
+		onboarded: true
+	}, {
+		multi: true
+	}, function (err, numReplaced) {
 		console.log('Deleted ', numReplaced, ' onboarding files.');
 	});
 }
 
 app.on('ready', function () {
 	db.loadDatabase();
-	db.find({ onboarded: true}, function(err, docs) {
+	resetOnboarding(); // For testing
+	db.find({
+		onboarded: true
+	}, function (err, docs) {
 		if (docs.length > 0) {
 			loadMenuBar();
-			resetOnboarding();
 		} else {
 			loadOnboarding();
 		}
