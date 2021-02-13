6c475386e32c8203dc2b9ba46858abf3b8d007b9
Parthiv Chigurupati <parthiv.chigurupati@gmail.com>
2021-02-12 22:37:29 -0500

    camera preview shows up, but menubar functionality is lost

--- menubar-master/myApp.js
+++ menubar-master/myApp.js
@@ -1,8 +1,26 @@
 const { menubar } = require('menubar');
+const {app, BrowserWindow} = require('electron');
 
 const mb = menubar();
 
 mb.on('ready', () => {
   console.log('app is ready');
-  // your app code here
+  // Create the browser window.
+    mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {nodeIntegration: true}});
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 });
+
+mb.on('window-all-closed', function() {
+    if (process.platform != 'darwin')
+      mb.quit();
+  });
