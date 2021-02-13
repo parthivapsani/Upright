377650c3922b7a89a70790f1fc7eedbeac4a547b
Pranav Wadhwa <pranavwadhwa@outlook.com>
2021-02-12 23:29:02 -0500

    Added process.js

--- upright-app/src/camera.js
+++ upright-app/src/camera.js
@@ -1,6 +1,10 @@
+import { process } from './process.js';
+
 navigator.mediaDevices.getUserMedia({video: true})
   .then(function(stream) {
     document.getElementById('camera').srcObject = stream;
+    process(stream);
+
   }).catch(function() {
     alert('could not connect stream');
   });
