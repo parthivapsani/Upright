ec8aabb4df2ccc23bda3f0c52cd1933f85e5af84
Parthiv Chigurupati <parthiv.chigurupati@gmail.com>
2021-02-13 01:01:31 -0500

    started frameGrab

--- upright-app/src/process.js
+++ upright-app/src/process.js
@@ -1,5 +1,21 @@
-function process(data) {
+function process(data, canvas) {
     console.log('processing: ', data);
+    console.log("process: ", data.getVideoTracks());
+    const track = data.getVideoTracks()[0];
+    let imageCapture = new ImageCapture(track);
+    console.log("here");
+    imageCapture.grabFrame().then(imageBitmap => {
+        console.log('Frame grabbed: ', imageBitmap);
+        canvas.width = imageBitmap.width;
+        canvas.height = imageBitmap.height;
+        console.log(canvas);
+        canvas.getContext('2d').drawImage(imageBitmap, 0, 0);
+    })
+    .catch(err => console.error('takePhoto() failed: ', err));
+    console.log("here");
+    // console.log("Took photo: ", photoBlob);
 }
 
 export { process }
+
+
