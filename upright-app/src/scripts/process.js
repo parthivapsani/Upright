ec6496df4d49ecc7b08ab2036d61bc05d4fb4ebb
vbhaip <vbhaip01@gmail.com>
2021-02-13 18:36:18 -0500

    new slouch calc invariant to distance from camera

--- upright-app/src/scripts/process.js
+++ upright-app/src/scripts/process.js
@@ -4,9 +4,20 @@ let filename = "poseData.json";
 let id = 0;
 let keypointIndices = [0, 1, 2, 5, 6];
 
+//from 0 to 1
+//0 means it'll say ur always slouching
+//1 means you'll basically never be slouching
+let sensitivity= 0.1
+
 let baseline = null;
 let shoulderbase = null;
 let latestimg;
+
+
+function dist(x1, y1, x2, y2){
+	return Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1 - y2, 2))
+
+}
 
 async function startup() {
 	net = await posenet.load();
@@ -28,22 +39,34 @@ async function estimate(image) {
 
 	let nose = pose["keypoints"][0]["position"]["y"]
 	let shoulder = (pose["keypoints"][5]["position"]["y"] + pose["keypoints"][6]["position"]["y"])/2
-    let shoulderWidth = (pose["keypoints"][5]["position"]["x"] - pose["keypoints"][6]["position"]["x"])
+
+	let eye_l = pose["keypoints"][1]["position"];
+	let eye_r = pose["keypoints"][2]["position"];
+
+	let eyeWidth = dist(eye_l['x'], eye_l['y'], eye_r['x'], eye_r['y'])
+
+	let shoulder_l = pose["keypoints"][5]["position"]
+	let shoulder_r = pose["keypoints"][6]["position"]
+
+    let shoulderWidth = dist(shoulder_l['x'], shoulder_l['y'], shoulder_r['x'], shoulder_r['y'])
     let metric = shoulder - nose;
 	// nose = nose / 480;
 	// shoulder = shoulder / 480;
+	//
+	let metric2 = eyeWidth/shoulderWidth;
 
 
 	if(baseline === null){
 		baseline = metric;
         shoulderbase = shoulderWidth;
+		baseline = metric2;
 	}
 
 	console.log("slouch index:");
-    console.log(baseline - metric);
-    console.log(shoulderbase - shoulderWidth);
+    console.log(metric2);
+    //console.log(shoulderbase - shoulderWidth);
 
-	if(shoulder - nose > baseline * 1.1 || shoulder - nose < baseline*0.9){
+	if(metric2 > baseline * (1 + sensitivity) || metric2 < baseline*(1-sensitivity)){
 		console.log("you're slouching")
 	}
 }
