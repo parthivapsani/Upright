8f87104cdd0c322e14b178f6382149dc6032ab84
Pranav Wadhwa <pranavwadhwa@outlook.com>
2021-02-12 22:46:19 -0500

    Showing camera on menu bar too

--- menubar-master/myApp.js
+++ menubar-master/myApp.js
@@ -1,26 +1,43 @@
-const { menubar } = require('menubar');
-const {app, BrowserWindow} = require('electron');
+const {
+	menubar
+} = require('menubar');
+const {
+	app,
+	BrowserWindow
+} = require('electron');
 
-const mb = menubar();
+app.on('ready', function () {
+	var mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
 
-mb.on('ready', () => {
-  console.log('app is ready');
-  // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {nodeIntegration: true}});
+	// and load the index.html of the app.
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
 
-    // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-        mainWindow = null;
-    });
-});
+	const mb = menubar({
+		browserWindow: mainWindow
+	});
 
-mb.on('window-all-closed', function() {
-    if (process.platform != 'darwin')
-      mb.quit();
-  });
+	mb.on('ready', () => {
+		console.log('app is ready');
+		// Create the browser window.
+
+	});
+
+	mb.on('window-all-closed', function () {
+		if (process.platform != 'darwin')
+			mb.quit();
+	});
+})
